e6c469abf2c65e13ead2d16be9b9069472d43cb7
“bitch” <bitch@gmial.com>
2016-08-21 21:33:46 +0800

    添加网络请求错误弹框

--- src/main/java/com/drcosu/ndileber/tools/net/RetCallback.java
+++ src/main/java/com/drcosu/ndileber/tools/net/RetCallback.java
@@ -1,8 +1,13 @@
 package com.drcosu.ndileber.tools.net;
 
 import com.drcosu.ndileber.app.SApplication;
+import com.drcosu.ndileber.tools.HJson;
+import com.drcosu.ndileber.tools.UDialog;
 import com.orhanobut.logger.Logger;
 
+import org.apache.http.HttpException;
+
+import java.net.ConnectException;
 import java.util.Set;
 
 import okhttp3.Headers;
@@ -17,6 +22,20 @@ import retrofit2.Response;
  */
 public abstract class RetCallback<T> implements Callback<T>{
 
+    //出错提示
+    public static String networkMsg = "网络请求失败,请检查";
+    public static String parseMsg;
+    public static String unknownMsg;
+
+    //对应HTTP的状态码
+    private static final int UNAUTHORIZED = 401;
+    private static final int FORBIDDEN = 403;
+    private static final int NOT_FOUND = 404;
+    private static final int REQUEST_TIMEOUT = 408;
+    private static final int INTERNAL_SERVER_ERROR = 500;
+    private static final int BAD_GATEWAY = 502;
+    private static final int SERVICE_UNAVAILABLE = 503;
+    private static final int GATEWAY_TIMEOUT = 504;
 
     protected abstract void onSuccess(Call<T> call, Response<T> response);
 
@@ -31,6 +50,13 @@ public abstract class RetCallback<T> implements Callback<T>{
     @Override
     public void onFailure(Call<T> call, Throwable throwable) {
         RetLog.log(call);
+        Throwable t = throwable;
+        while (t != null) {
+            if (t instanceof ConnectException) {
+                UDialog.alert(UDialog.DIALOG_ERROR,networkMsg).show();
+            }
+            t = t.getCause();
+        }
         failure(call,throwable);
     }
 
