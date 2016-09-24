302dcf54f59a0294a5d14053c654e15737ff90e0
sdw <297165331@qq.com>
2016-09-24 10:08:26 +0800

    添加 presenter基类

---
+++ src/main/java/com/drcosu/ndileber/mvp/presenter/DileberPresenter.java
@@ -0,0 +1,23 @@
+package com.drcosu.ndileber.mvp.presenter;
+
+import android.support.annotation.NonNull;
+
+import com.drcosu.ndileber.mvp.data.BaseDataSource;
+import com.drcosu.ndileber.mvp.view.BaseView;
+
+/**
+ * Created by shidawei on 2016/9/24.
+ */
+public abstract class DileberPresenter<T1 extends BaseView<BasePresenter>,T2 extends BaseDataSource> implements BasePresenter {
+
+
+    T1 mView;
+    T2 mUserDataSource;
+
+    public DileberPresenter(@NonNull T1 view, @NonNull T2 userDataSource){
+        mView = view;
+        mUserDataSource = userDataSource;
+        mView.setPresenter(this);
+    }
+
+}
