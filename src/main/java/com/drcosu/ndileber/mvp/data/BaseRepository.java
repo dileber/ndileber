273ac8a68d603a2f3f319220af49f3af50e83c36
sdw <297165331@qq.com>
2016-10-09 14:45:20 +0800

    添加刷新缓存的标志位

--- src/main/java/com/drcosu/ndileber/mvp/data/BaseRepository.java
+++ src/main/java/com/drcosu/ndileber/mvp/data/BaseRepository.java
@@ -18,12 +18,29 @@ public class BaseRepository<T1 extends BaseLocalDataSource,T2 extends BaseRemote
 
     protected Map<String,Object> cache = null;
 
+    /**
+     * 是否刷新缓存
+     */
+    private boolean refCache = false;
+
     protected BaseRepository(T1 localDataSource, T2 remoteDataSource) {
         this.localDataSource = localDataSource;
         this.remoteDataSource = remoteDataSource;
         if(BaseConfiger.BUG_STATIC){
             cache = new DebugNoCacheMap();
         }
+    }
+
+    public void refOver(){
+        refCache = false;
+    }
+
+    public void refStart(){
+        refCache = true;
+    }
+
+    public boolean isRefCache(){
+        return refCache;
     }
 
 }
