22ae9fc9244631865f3f202e77ffd828279ddd46
sdw <297165331@qq.com>
2016-10-09 23:18:29 +0800

    修改bug，对数据库管理类增加更新后数据表删除的代码

--- src/main/java/com/drcosu/ndileber/mvp/data/source/local/DBHelper.java
+++ src/main/java/com/drcosu/ndileber/mvp/data/source/local/DBHelper.java
@@ -1,9 +1,13 @@
 package com.drcosu.ndileber.mvp.data.source.local;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,9 +45,24 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * android_metadata是默认的表
+     * 当SQLite数据库中包含自增列时，会自动建立一个名为 sqlite_sequence 的表。
+     * @param sqLiteDatabase
+     * @param i
+     * @param i1
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        Logger.sl(Log.DEBUG,"数据库版本有变动","DROP TABLE IF EXISTS " ,DATABASE_NAME,i," fdsa ",i1);
+        Cursor cursor = sqLiteDatabase.rawQuery("select name from sqlite_master where type='table' and name !='android_metadata' and name!='sqlite_sequence' order by name", null);
+        while(cursor.moveToNext()){
+            //遍历出表名
+            String name = cursor.getString(0);
+            Logger.sl(Log.DEBUG,"数据库表名",name);
+
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + name);
+        }
         onCreate(sqLiteDatabase);
     }
 }
