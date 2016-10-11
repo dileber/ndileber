fc3fa6b62af08d6e74a0f03c6f784a49d8f3cfba
sdw <297165331@qq.com>
2016-10-11 12:15:28 +0800

    添加fragment 新方法，替换，或者 切换

--- src/main/java/com/drcosu/ndileber/utils/ActivityUtils.java
+++ src/main/java/com/drcosu/ndileber/utils/ActivityUtils.java
@@ -64,4 +64,61 @@ public class ActivityUtils {
         return fragment;
     }
 
+    /**
+     * 如果原来的fragment不再使用,可用如下方法用新的替换旧的,如果你的fragment依旧要使用的话 用 {@link #switchFragment(FragmentManager, int, Fragment, Fragment)}  }
+     * @param fragmentManager
+     * @param fragmentId
+     * @param myFragment
+     * @param <T>
+     * @return
+     */
+    public static  <T extends Fragment>T replaceFragment(@NonNull FragmentManager fragmentManager, @NonNull int fragmentId, @NonNull T myFragment){
+        Check.checkNotNull(fragmentManager);
+        Check.checkNotNull(fragmentId);
+        Check.checkNotNull(myFragment);
+        T fragment = (T)fragmentManager.findFragmentById(fragmentId);
+        if (fragment == null) {
+            fragment = myFragment;
+            addFragmentToActivity(fragmentManager, fragment,fragmentId);
+        }else{
+            fragment = myFragment;
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(fragmentId, fragment);
+            transaction.commit();
+        }
+        return fragment;
+    }
+
+    /**
+     * 切换 fragment 显示
+     * @param fragmentManager
+     * @param fragmentId
+     * @param from
+     * @param to
+     * @param <T1>
+     * @param <T2>
+     * @return
+     */
+    public static <T1 extends Fragment,T2 extends Fragment>T2  switchFragment(@NonNull FragmentManager fragmentManager, @NonNull int fragmentId,  T1 from,@NonNull T2 to) {
+        Check.checkNotNull(fragmentManager);
+        Check.checkNotNull(fragmentId);
+        Check.checkNotNull(to);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        if(from==null){
+            if(!to.isAdded()){
+                transaction.add(fragmentId, to).commit();
+            }else{
+                transaction.show(to).commit();
+            }
+        }else{
+            if(!to.isAdded()){
+                transaction.hide(from).add(fragmentId, to).commit();
+            }else{
+                transaction.hide(from).show(to).commit();
+            }
+        }
+        return to;
+    }
+
+
 }
