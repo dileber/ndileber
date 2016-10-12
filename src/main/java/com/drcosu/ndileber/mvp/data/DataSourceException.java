fcceaff19b1333406090fe0f2e8d7ca8324bfbc9
sdw <297165331@qq.com>
2016-10-12 11:01:34 +0800

    修改网络exception

--- src/main/java/com/drcosu/ndileber/mvp/data/DataSourceException.java
+++ src/main/java/com/drcosu/ndileber/mvp/data/DataSourceException.java
@@ -4,9 +4,16 @@ package com.drcosu.ndileber.mvp.data;
  * Created by shidawei on 16/8/4.
  */
 public class DataSourceException extends Exception{
-
+    private int code;
     public DataSourceException(String msg){
         super(msg);
     }
 
+    public DataSourceException(String msg,Integer code){
+        super(msg);
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
