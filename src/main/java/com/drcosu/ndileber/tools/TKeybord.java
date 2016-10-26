ff3ddb936576155dcaeef2cbdc2a72ed7b0d1304
sdw <297165331@qq.com>
2016-10-26 16:57:13 +0800

    使用InputMethodManager会导致内存溢出，添加销毁方法

--- src/main/java/com/drcosu/ndileber/tools/TKeybord.java
+++ src/main/java/com/drcosu/ndileber/tools/TKeybord.java
@@ -1,10 +1,13 @@
 package com.drcosu.ndileber.tools;
 
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import com.orhanobut.logger.Logger;
+
+import java.lang.reflect.Field;
 
 /**
  * 键盘工具
@@ -37,6 +40,46 @@ public class TKeybord {
         Logger.d("closeKeybord");
         InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+    /**
+     * 销毁键盘时候调用
+     * @param destContext
+     */
+    public static void fixInputMethodManagerLeak(Context destContext) {
+        if (destContext == null) {
+            return;
+        }
+
+        InputMethodManager imm = (InputMethodManager) destContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) {
+            return;
+        }
+
+        String [] arr = new String[]{"mCurRootView", "mServedView", "mNextServedView"};
+        Field f = null;
+        Object obj_get = null;
+        for (int i = 0;i < arr.length;i ++) {
+            String param = arr[i];
+            try{
+                f = imm.getClass().getDeclaredField(param);
+                if (f.isAccessible() == false) {
+                    f.setAccessible(true);
+                } // author: sodino mail:sodino@qq.com
+                obj_get = f.get(imm);
+                if (obj_get != null && obj_get instanceof View) {
+                    View v_get = (View) obj_get;
+                    if (v_get.getContext() == destContext) { // 被InputMethodManager持有引用的context是想要目标销毁的
+                        f.set(imm, null); // 置空，破坏掉path to gc节点
+                    } else {
+                        // 不是想要目标销毁的，即为又进了另一层界面了，不要处理，避免影响原逻辑,也就不用继续for循环了
+                        break;
+                    }
+                }
+            }catch(Throwable t){
+                t.printStackTrace();
+            }
+        }
     }
 
 }
