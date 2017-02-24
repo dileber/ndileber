c35509ac23c01d2fbc4a361e5dc3a83a6cc2903c
sdw <297165331@qq.com>
2017-02-24 12:40:40 +0800

    添加分辨率方法

--- src/main/java/com/drcosu/ndileber/tools/UUi.java
+++ src/main/java/com/drcosu/ndileber/tools/UUi.java
@@ -82,4 +82,48 @@ public class UUi {
 
     }
 
+    /**
+     * dip 转 px
+     * @param context
+     * @param dipValue
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5F);
+    }
+
+    /**
+     * px 转 dp
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(pxValue / scale + 0.5F);
+    }
+
+    /**
+     * px转sp
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(pxValue / fontScale + 0.5F);
+    }
+
+    /**
+     * sp转px
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(spValue * fontScale + 0.5F);
+    }
+
 }
