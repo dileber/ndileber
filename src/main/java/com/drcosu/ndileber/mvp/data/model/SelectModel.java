65c51c17a21c3786125a6743edf3a8eb60075ab9
sdw <297165331@qq.com>
2017-02-25 12:02:19 +0800

    修改选择dialog model

--- src/main/java/com/drcosu/ndileber/mvp/data/model/SelectModel.java
+++ src/main/java/com/drcosu/ndileber/mvp/data/model/SelectModel.java
@@ -5,6 +5,16 @@ package com.drcosu.ndileber.mvp.data.model;
  */
 public class SelectModel extends SModel{
 
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private String title;
 
     public void setTitle(String title) {
